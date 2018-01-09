e37ed7455111dbc28a9c4573b9aa3746a7cbf8dd
Aiman Asyraf Bin Anuar <imbacakes@gmail.com>
2018-01-09 16:55:13 +0800

    setup react

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,10 @@
 import livereload from "rollup-plugin-livereload";
-import serve from "rollup-plugin-serve";
+import serve      from "rollup-plugin-serve";
+import babel      from "rollup-plugin-babel";
+import commonjs   from "rollup-plugin-commonjs";
+import resolve    from "rollup-plugin-node-resolve";
+import globals    from "rollup-plugin-node-globals";
+
 export default {
   input: "src/main.js",
   output: {
@@ -7,6 +12,28 @@ export default {
     format: "iife"
   },
   plugins: [
+    babel({
+      babelrc: false,
+      exclude: "node_modules/**",
+      presets: [["es2015", {modules: false}], "stage-0", "react"],
+      plugins: ["external-helpers"]
+    }),
+    commonjs({
+      exclude: "node_modules/process-es6/**",
+      include: [
+        "node_modules/create-react-class/**",
+        "node_modules/fbjs/**",
+        "node_modules/object-assign/**",
+        "node_modules/react/**",
+        "node_modules/react-dom/**",
+        "node_modules/prop-types/**"
+      ]
+    }),
+    globals(),
+    resolve({
+      browser: true,
+      main: true
+    }),
     serve("dist"),
     livereload("dist")
   ]
--- src/main.js
+++ src/main.js
@@ -1,2 +1,8 @@
-console.log("hello aiman");
-console.log("reload?");
+import React from "react"
+import ReactDOM from "react-dom"
+
+import { WelcomeComponent } from "./welcome_component.js"
+
+const root = document.querySelector("main")
+
+ReactDOM.render(<WelcomeComponent />, root)
